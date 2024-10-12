dff49c8517b8fd4d8c0b38d77544037dcda4ef1a
Juleshaydn <63356614+Juleshaydn@users.noreply.github.com>
2024-10-12 17:05:13 +0100

    syncs to database

--- db.py
+++ db.py
@@ -5,25 +5,17 @@ import os
 
 def get_db_connection():
     conn = psycopg2.connect(
-        host=os.getenv("DB_HOST", "db"),
-        port=os.getenv("DB_PORT", "5432"),
-        dbname=os.getenv("DB_NAME", "stock_price_db"),
-        user=os.getenv("DB_USER", "your_db_username"),
-        password=os.getenv("DB_PASSWORD", "your_secure_password")
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
     return conn
 
 def create_tables():
     conn = get_db_connection()
     cursor = conn.cursor()
-    # Create table for stock prices
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS stock_prices (
-        symbol VARCHAR(10) PRIMARY KEY,
-        price REAL,
-        timestamp TIMESTAMP
-    );
-    """)
     # Create or alter the historical_stock_data table
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS historical_stock_data (
